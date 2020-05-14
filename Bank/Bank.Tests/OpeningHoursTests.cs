2392a099b4d7c40b58921544b45c5bb99d9aa3e3
André <ruland@outlook.de>
2020-05-14 16:13:12 +0200

    Bank mit TDD

---
+++ Bank/Bank.Tests/OpeningHoursTests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Bank.Tests
+{
+    public class OpeningHoursTests
+    {
+        [Theory]
+        [InlineData(2020, 1, 1, 10, 29, false)] //Mi
+        [InlineData(2020, 1, 1, 10, 30, true)] //Mi
+        [InlineData(2020, 1, 1, 10, 31, true)] //Mi
+        [InlineData(2020, 1, 1, 18, 59, true)] //Mi
+        [InlineData(2020, 1, 1, 19, 0, false)] //Mi
+        [InlineData(2020, 1, 1, 19, 1, false)] //Mi
+        [InlineData(2020, 1, 4, 10, 29, !true)] //Sa
+        [InlineData(2020, 1, 4, 10, 30, true)] //Sa
+        [InlineData(2020, 1, 4, 10, 31, true)] //Sa
+        [InlineData(2020, 1, 4, 13, 59, true)] //Sa
+        [InlineData(2020, 1, 4, 14, 00, !true)] //Sa
+        [InlineData(2020, 1, 5, 12, 00, !true)] //So
+        public void OpeningHours_IsOpen(int year, int month, int day, int hour, int minute, bool exp)
+        {
+            var oh = new OpeningHours();
+            var dt = new DateTime(year, month, day, hour, minute, 0);
+
+            Assert.Equal(exp, oh.IsOpen(dt));
+        }
+    }
+}
---
+++ Bank/Bank/OpeningHours.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Bank
+{
+    public class OpeningHours
+    {
+        private Dictionary<DayOfWeek, (TimeSpan Start, TimeSpan End)> days = new Dictionary<DayOfWeek, (TimeSpan, TimeSpan)>();
+
+        public OpeningHours()
+        {
+            days.Add(DayOfWeek.Monday, (new TimeSpan(10, 30, 0), new TimeSpan(19, 0, 0)));
+            days.Add(DayOfWeek.Tuesday, (new TimeSpan(10, 30, 0), new TimeSpan(19, 0, 0)));
+            days.Add(DayOfWeek.Wednesday, (new TimeSpan(10, 30, 0), new TimeSpan(19, 0, 0)));
+            days.Add(DayOfWeek.Thursday, (new TimeSpan(10, 30, 0), new TimeSpan(19, 0, 0)));
+            days.Add(DayOfWeek.Friday, (new TimeSpan(10, 30, 0), new TimeSpan(19, 0, 0)));
+            days.Add(DayOfWeek.Saturday, (new TimeSpan(10, 30, 0), new TimeSpan(14, 0, 0)));
+        }
+
+        public bool IsOpen(DateTime time)
+        {
+            return
+                   days.ContainsKey(time.DayOfWeek) &&
+                   days[time.DayOfWeek].Start <= time.TimeOfDay &&
+                   days[time.DayOfWeek].End > time.TimeOfDay;
+        }
+
+        public bool IsOpenNow()
+        {
+            return IsOpen(DateTime.Now);
+        }
+    }
+
+}
