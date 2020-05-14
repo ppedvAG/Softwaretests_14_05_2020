cad96a1ae77a741feb66f6b55577e1412a252743
André <ruland@outlook.de>
2020-05-14 17:05:23 +0200

    Tests Mit Fakes

--- Datehelper/Datehelper/Datehelper.cs
+++ Datehelper/Datehelper/Datehelper.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 namespace Tools
 {
@@ -8,6 +9,11 @@ namespace Tools
         {
             return DateTime.Now.DayOfWeek == DayOfWeek.Sunday
                 || DateTime.Now.DayOfWeek == DayOfWeek.Saturday;
+        }
+
+        public void DummesZeug()
+        {
+            var data = File.ReadAllText(@"ab:\alledatein\lala.txt");
         }
     }
 }
--- Datehelper/Tools.Tests/DatehelperTests.cs
+++ Datehelper/Tools.Tests/DatehelperTests.cs
@@ -1,6 +1,8 @@
 ﻿using System;
+using System.IO;
 using Microsoft.QualityTools.Testing.Fakes;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Pose;
 
 namespace Tools.Tests
 {
@@ -14,15 +16,48 @@ namespace Tools.Tests
 
             using (ShimsContext.Create())
             {
-                
+                System.Fakes.ShimDateTime.NowGet = () => new DateTime(2020, 5, 11);
                 Assert.IsFalse(dh.IsWeekend());//Mo
+                System.Fakes.ShimDateTime.NowGet = () => new DateTime(2020, 5, 12);
                 Assert.IsFalse(dh.IsWeekend());//Di
+                System.Fakes.ShimDateTime.NowGet = () => new DateTime(2020, 5, 13);
                 Assert.IsFalse(dh.IsWeekend());//Mi
+                System.Fakes.ShimDateTime.NowGet = () => new DateTime(2020, 5, 14);
                 Assert.IsFalse(dh.IsWeekend());//Do
+                System.Fakes.ShimDateTime.NowGet = () => new DateTime(2020, 5, 15);
                 Assert.IsFalse(dh.IsWeekend());//Fr
+                System.Fakes.ShimDateTime.NowGet = () => new DateTime(2020, 5, 16);
                 Assert.IsTrue(dh.IsWeekend());//Sa
+                System.Fakes.ShimDateTime.NowGet = () => new DateTime(2020, 5, 17);
                 Assert.IsTrue(dh.IsWeekend());//So
             }
+        }
+
+        [TestMethod]
+        public void DateHelper_Dummes_Tests()
+        {
+            var dh = new Datehelper();
+            using (ShimsContext.Create())
+            {
+                System.IO.Fakes.ShimFile.ReadAllTextString = x => "RESULT";
+                dh.DummesZeug();
+            }
+        }
+
+
+        [TestMethod]
+        public void DateHelper_Dummes_Tests_POSE()
+        {
+            var dh = new Datehelper();
+            PoseContext.Isolate(() =>
+            {
+                Shim consoleShim = Shim.Replace(() => File.ReadAllText(Is.A<string>())).With(
+    delegate (string s) { return "LALALAA"; });
+
+
+                dh.DummesZeug();
+            });
+
         }
     }
 }
