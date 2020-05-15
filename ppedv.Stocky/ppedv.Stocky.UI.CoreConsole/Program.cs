9a80ab99854d2c63b9823908a44bce262a4504b0
André <ruland@outlook.de>
2020-05-15 15:56:31 +0200

    Consolen UI FTW

---
+++ ppedv.Stocky/ppedv.Stocky.UI.CoreConsole/Program.cs
@@ -0,0 +1,26 @@
+﻿using ppedv.Stocky.Logic;
+using ppedv.Stocky.Model;
+using System;
+using System.Linq;
+
+namespace ppedv.Stocky.UI.CoreConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("*** Stocky v0.1 ***");
+
+            var core = new Core();
+
+            foreach (var b in core.Repository.GetAll<Bulk>())
+            {
+                Console.WriteLine($"{b.Type} {b.Storage.Sum(x => x.Menge)}kg");
+            }
+
+
+            Console.WriteLine("Ende");
+            Console.ReadLine();
+        }
+    }
+}
