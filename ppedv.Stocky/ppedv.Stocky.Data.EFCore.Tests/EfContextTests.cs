9aca3416c07d07886c8a8e55aef63f63b0282897
André <ruland@outlook.de>
2020-05-15 11:26:22 +0200

    CRUD test für Stock

--- ppedv.Stocky/ppedv.Stocky.Data.EFCore.Tests/EfContextTests.cs
+++ ppedv.Stocky/ppedv.Stocky.Data.EFCore.Tests/EfContextTests.cs
@@ -1,5 +1,6 @@
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Storage;
+using ppedv.Stocky.Model;
 using System;
 using Xunit;
 
@@ -18,5 +19,53 @@ namespace ppedv.Stocky.Data.EFCore.Tests
 
             Assert.True((con.Database.GetService<IDatabaseCreator>() as RelationalDatabaseCreator).Exists());
         }
+
+        [Fact]
+        public void Can_CRUD_Stock()
+        {
+            var stock = new Stock() { Name = $"Teststock_{Guid.NewGuid()}" };
+            var newName = $"UpdateStock_{Guid.NewGuid()}";
+
+            //CREATE
+            using (var con = new EfContext())
+            {
+                con.Stocks.Add(stock);
+                con.SaveChanges();
+            }
+
+            //READ
+            using (var con = new EfContext())
+            {
+                var loaded = con.Stocks.Find(stock.Id);
+                Assert.NotNull(loaded);
+                Assert.Equal(stock.Name, loaded.Name);
+
+                //UPDATE
+                loaded.Name = newName;
+                con.SaveChanges();
+            }
+
+            //check UPDATE
+            using (var con = new EfContext())
+            {
+                var loaded = con.Stocks.Find(stock.Id);
+                Assert.Equal(newName, loaded.Name);
+
+                //DELETE
+                con.Stocks.Remove(loaded);
+                con.SaveChanges();
+            }
+
+            //check DELETE
+            using (var con = new EfContext())
+            {
+                var loaded = con.Stocks.Find(stock.Id);
+                Assert.Null(loaded);
+
+            }
+
+        }
+
+
     }
 }
